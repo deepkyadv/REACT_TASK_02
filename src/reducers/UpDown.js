559c592999bfa2468f7fb44df37ccb7e2913fc69
Deepak Yadav <132565298+deepkyadv@users.noreply.github.com>
2024-06-25 10:48:08 +0530

    Update UpDown.js

--- src/reducers/UpDown.js
+++ src/reducers/UpDown.js
@@ -2,7 +2,7 @@ const initialState = 0;
 const changeTheNumber = (state = initialState, action)=>{
     switch(action.type){
         case "INCREMENT": return state+1;
-        case "DECREMENT": return state > 0 ? state - 1 : state;
+        case "DECREMENT": return state-1;
         default: return state;
 
     }
